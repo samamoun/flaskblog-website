48e25a664a77b3e1923c0ec59660bf12c0bb400e
samamoun <46803937+samamoun@users.noreply.github.com>
2020-09-15 04:15:06 -0400

    app routes and blog posts

---
+++ routes.py
@@ -0,0 +1,124 @@
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+posts = [
+    {
+        'author': 'Sarah Al Mamoun',
+        'title': 'Blog Post #1',
+        'content': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+                   'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,'
+                   ' when an unknown printer took a galley of type and scrambled it to make a type '
+                   'specimen book. It has survived not only five centuries, but also the leap into '
+                   'electronic typesetting, remaining essentially unchanged. ',
+        'date_posted': 'April 14, 2020'
+    },
+    {
+        'author': 'Sarah Al Mamoun',
+        'title': 'Blog Post #2',
+        'content': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+                   'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,'
+                   ' when an unknown printer took a galley of type and scrambled it to make a type '
+                   'specimen book. It has survived not only five centuries, but also the leap into '
+                   'electronic typesetting, remaining essentially unchanged. ',
+        'date_posted': 'April 13, 2020'
+    },
+    {
+        'author': 'Sarah Al Mamoun',
+        'title': 'Blog Post #3',
+        'content': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+                   'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,'
+                   ' when an unknown printer took a galley of type and scrambled it to make a type '
+                   'specimen book. It has survived not only five centuries, but also the leap into '
+                   'electronic typesetting, remaining essentially unchanged. ',
+        'date_posted': 'April 13, 2020'
+    },
+    {
+        'author': 'Sarah Al Mamoun',
+        'title': 'Blog Post #4',
+        'content': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+                   'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,'
+                   ' when an unknown printer took a galley of type and scrambled it to make a type '
+                   'specimen book. It has survived not only five centuries, but also the leap into '
+                   'electronic typesetting, remaining essentially unchanged. ',
+        'date_posted': 'April 13, 2020'
+    },
+    {
+        'author': 'Sarah Al Mamoun',
+        'title': 'Blog Post #5',
+        'content': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. '
+                   'Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,'
+                   ' when an unknown printer took a galley of type and scrambled it to make a type '
+                   'specimen book. It has survived not only five centuries, but also the leap into '
+                   'electronic typesetting, remaining essentially unchanged. ',
+        'date_posted': 'April 13, 2020'
+    }
+
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/work")
+def work():
+    return render_template('work.html', title='Work')
+
+
+@app.route("/contact")
+def contact():  # call method about
+    return render_template("Contact.html")  # Contact page
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
